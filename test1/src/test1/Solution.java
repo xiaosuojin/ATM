4381faa523503cdbce2c5f24745d263e5f0db538
xiaosuojin <93508177+xiaosuojin@users.noreply.github.com>
2021-11-01 14:06:26 +0800

    Add files via upload

---
+++ test1/src/test1/Solution.java
@@ -0,0 +1,12 @@
+package test1;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        if ( System . out . printf ( "Hello, World.\nHello, Java." ) != null ){
+        	
+        }
+        
+    }
+}
---
+++ test1/src/test1/X2.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class X2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] abc = new String[3]{"a","b","c"};
+        for (String str :abc){
+            System .out.print(str);
+        }
+
+	}
+
+}
